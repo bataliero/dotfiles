1a949ed6a50d1bf8f6478a81d31a2f3accb939e2
bataliero <bataliero.pl@gmail.com>
2021-09-12 22:18:49 +0200

    aa

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+bind \e\[1\;5C forward-word
+bind \e\[1\;5D backward-word
+bind \ci nextd-or-forward-word                                  
+bind \co prevd-or-backward-word
+
+set EDITOR vi
+bind \t complete
