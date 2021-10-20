7571f5f9d45ba9e099c13ecf351518aed3a84f0c
Piotr Kleban <piotr.kleban@streamvx.com>
2021-10-20 10:22:56 +0200

    minor update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,13 @@
-bind \e\[1\;5C forward-word
-bind \e\[1\;5D backward-word
-bind \ci nextd-or-forward-word                                  
-bind \co prevd-or-backward-word
+bind \ei nextd-or-forward-word                                  
+bind \eo prevd-or-backward-word
+
+# alt+shift+u:
+bind -a \eu '..;commandline -f repaint'
+bind -a \eU '..;commandline -f repaint'
+
+bind \en history-token-search-forward 
+bind \ep history-token-search-backward
+
 
 set EDITOR vi
 bind \t complete
